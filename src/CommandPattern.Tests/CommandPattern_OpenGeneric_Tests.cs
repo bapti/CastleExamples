f7156591504fe94ec5d34a4b9471022605a54faa
Neil Crawford <neil.crawford@gmail.com>
2014-11-26 21:34:09 +0000

    A working and a broken test

---
+++ src/CommandPattern.Tests/CommandPattern_OpenGeneric_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using CommandPattern.Commands.Accelerate;
+using CommandPattern.Infrastructure;
+using TypedFactory.Ioc;
+using Xunit;
+using FluentAssertions;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
+using CommandPattern.Handlers;
+
+namespace CommandPattern.Tests
+{
+    public class CommandPattern_OpenGeneric_Tests
+    {
+        private readonly CommandProcessor sut;
+
+        public CommandPattern_OpenGeneric_Tests()
+        {
+            var container = new WindsorContainer();
+            container.AddFacility<TypedFactoryFacility>();
+
+            container.Register(
+                Component
+                    .For(typeof(ICommandHandler<>))
+                    .ImplementedBy(typeof(LoggingDecorator<>))
+                    .LifestyleTransient(),
+
+                Component
+                    .For(typeof(ICommandHandler<>))
+                    .ImplementedBy(typeof(TimingDecorator<>))
+                    .LifestyleTransient(),
+
+                Classes
+                    .FromThisAssembly()
+                    .BasedOn(typeof(ICommandHandler<>))
+                    .WithServiceFromInterface()
+                    .LifestyleTransient(),
+
+                //Component
+                //    .For(typeof(ICommandHandler<AccelerateCommand>))
+                //    .ImplementedBy(typeof(AccelerateCommandHandler))
+                //    .LifestyleTransient(),   
+
+                Component.For<ICommandHandlerFactory>().AsFactory(),
+                Component.For<IMetrics>().ImplementedBy<Metrics>(),
+                Component.For<ILogger>().ImplementedBy<Logger>(),
+                Component.For<CommandProcessor>()
+                );
+
+            sut = container.Resolve<CommandProcessor>();
+        }
+
+        [Fact]
+        public void Should_Run_Accelerate_Through_Logging_Timing_Then_Accelerate()
+        {
+            var data = new AccelerateCommand();
+
+            sut.ProcessCommand<AccelerateCommand>(data);
+
+            data.Log[0].Should().Be("logging");
+            data.Log[1].Should().Be("timing");
+            data.Log[2].Should().Be("accelerate");
+        }
+    }
+}
